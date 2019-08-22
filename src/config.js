feca3e2fa2c4684453c1d3ce4f29cf5566136b39
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-08-23 00:51:41 +0300

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export const taskConfig = {
+  dataPeriod: {
+    past: -7,
+    future: 7
+  },
+  tagsAmount: {
+    minAmount: 0,
+    maxAmount: 3
+  }
+};
+
+export const tasksConfig = {
+  perLoad: 8,
+  minAmount: 30,
+  maxAmount: 20
+};
