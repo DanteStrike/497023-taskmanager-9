936539342e48d51a9fcb14a2d795f0501e0b10c2
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-08-23 00:52:54 +0300

    REFACTOR main.js p1

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+import * as data from './data.js';
+import {tasksConfig} from './config.js';
+import {Position, createElement, render, hideElement} from './utils/utils.js';
 import {getMenuTemplate} from './components/menu.js';
 import {getSearchTemplate} from './components/search.js';
 import {createFiltersTemplate} from './components/filters.js';
@@ -6,70 +9,46 @@ import {createBoardFilter} from './components/board-filter.js';
 import {createTaskCard} from './components/task.js';
 import {createTaskEditForm} from './components/task-edit.js';
 import {createLoadMoreButton} from './components/load-more-button.js';
-import {tasksData} from './data.js';
-import {filtersData} from './data.js';
-
-
-const TASKS_PER_LOAD = 8;
-
-
-const hideElement = (element) => {
-  element.style.display = `none`;
-};
-
-/**
- * Зарендерить компонент на страницу.
- *
- * @param {object} node - родительский Node, вставляемого компонента.
- * @param {string} markup - вставляемая разметка.
- * @param {string} position - позиция вставки.
- */
-const renderComponent = (node, markup, position = `beforeend`) => {
-  node.insertAdjacentHTML(position, markup);
-};
-
-//  Рендер определенного кол-ва задач при нажатии
-let lastRenderTaskIndex = null;
-const onLoadButtonClick = () => {
-  renderComponent(tasksBoard, tasksData.slice(lastRenderTaskIndex, lastRenderTaskIndex + TASKS_PER_LOAD).map(createTaskCard).join(``));
-  lastRenderTaskIndex += TASKS_PER_LOAD;
-  if (lastRenderTaskIndex >= tasksData.length) {
-    hideElement(loadButton);
-    loadButton.removeEventListener(`click`, onLoadButtonClick);
-  }
-};
 
 //  Рендер меню, поиска и его фильтров
 const mainNode = document.querySelector(`main`);
 const mainControl = mainNode.querySelector(`.control`);
 
-renderComponent(mainControl, getMenuTemplate());
-renderComponent(mainNode, getSearchTemplate());
-renderComponent(mainNode, createFiltersTemplate(filtersData));
+render(mainControl, getMenuTemplate());
+render(mainNode, getSearchTemplate());
+render(mainNode, createFiltersTemplate(data.filters));
 
 //  Рендер доски задач и его фильтров
-renderComponent(mainNode, createTasksBoard());
+render(mainNode, createTasksBoard());
 
 const board = mainNode.querySelector(`.board`);
 const tasksBoard = board.querySelector(`.board__tasks`);
 
-renderComponent(board, createBoardFilter(), `afterbegin`);
+render(board, createBoardFilter(), `afterbegin`);
 
 //  Рендер первого набора задач
 let editTaskData;
 let firstInitTaskData;
-[editTaskData, ...firstInitTaskData] = tasksData.slice(0, TASKS_PER_LOAD);
-renderComponent(tasksBoard, createTaskEditForm(editTaskData));
-renderComponent(tasksBoard, firstInitTaskData.map(createTaskCard).join(``));
+[editTaskData, ...firstInitTaskData] = data.tasksList.slice(0, tasksConfig.perLoad);
+render(tasksBoard, createTaskEditForm(editTaskData));
+render(tasksBoard, firstInitTaskData.map(createTaskCard).join(``));
 
 //  Рендер кнопки загрузки
-renderComponent(board, createLoadMoreButton());
+render(board, createLoadMoreButton());
 
-let loadButton = board.querySelector(`.load-more`);
-
-if (tasksData.length <= TASKS_PER_LOAD) {
+const loadButton = board.querySelector(`.load-more`);
+if (data.tasksList.length <= tasksConfig.perLoad) {
   hideElement(loadButton);
 } else {
-  lastRenderTaskIndex = TASKS_PER_LOAD;
+  //  Рендер определенного кол-ва задач при нажатии
+  let lastRenderTaskIndex = tasksConfig.perLoad;
+  const onLoadButtonClick = () => {
+    render(tasksBoard, data.tasksList.slice(lastRenderTaskIndex, lastRenderTaskIndex + tasksConfig.perLoad).map(createTaskCard).join(``));
+    lastRenderTaskIndex += tasksConfig.perLoad;
+    if (lastRenderTaskIndex >= data.tasksList.length) {
+      hideElement(loadButton);
+      loadButton.removeEventListener(`click`, onLoadButtonClick);
+    }
+  };
   loadButton.addEventListener(`click`, onLoadButtonClick);
 }
