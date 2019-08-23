9eaad5df98d345a9866b7854eede4270f9a7b564
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-08-23 21:50:29 +0300

    ADD config file

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export const taskConfig = {
+  dataPeriod: {
+    past: -7,
+    future: 7
+  },
+  tags: {
+    minAmount: 0,
+    maxAmount: 3
+  }
+};
+
+export const tasksConfig = {
+  perLoad: 8,
+  minAmount: 20,
+  maxAmount: 30
+};
